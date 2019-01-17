b36add769b3796d1f2f49e1c963fca3e4e92b690
Jeff Rojas <jrojas3991@gmail.com>
2019-01-17 14:21:07 -0500

    updated py

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ from flask import render_template
 
 app = Flask (__name__)
 
-app.route('/')
-
+@app.route('/')
 def index():
-    return
+    return render_template('index.html')
+
+@app.route('/poem/<poem_id>/')
+
+def poem_now(poem_id):
+    poem_id = int(poem_id)
+
+    return render_template('poem_detail.html')
